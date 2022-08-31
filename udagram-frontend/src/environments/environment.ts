0113b7e6afa890a3cdfe672697bf2d628ebc9297
NetDef <netdef@pop-os.localdomain>
2022-08-31 11:34:59 +0300

    Final

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4efefd43e3e846d087ecbdffb654bee-564775695.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
